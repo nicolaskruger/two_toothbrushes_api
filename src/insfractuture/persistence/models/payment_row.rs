34dcd8020c19d117e363d7bb04e8650d76409144
ni <nicolas.nkruger@gmail.com>
2026-02-28 10:29:05 -0300

    save

--- src/insfractuture/persistence/models/payment_row.rs
+++ src/insfractuture/persistence/models/payment_row.rs
@@ -1,17 +1,17 @@
 use chrono::{DateTime, Utc};
 use uuid::Uuid;
 
-pub struct Payment {
+pub struct PaymentRow {
     pub id: Uuid,
     pub message: String,
     pub reais: f64,
-    pub status: PaymentStatus,
+    pub status: PaymentStatusRow,
     pub created_at: DateTime<Utc>,
 }
 
 #[derive(sqlx::Type)]
 #[sqlx(type_name = "payment_status", rename_all = "lowercase")]
-pub enum PaymentStatus {
+pub enum PaymentStatusRow {
     Pending,
     Approved,
     Rejected,
