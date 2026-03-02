20274ea6355ff10faddee7254158330a42132884
ni <nicolas.nkruger@gmail.com>
2026-03-02 06:11:01 -0300

    test

--- src/domain/repository/user_repository.rs
+++ src/domain/repository/user_repository.rs
@@ -4,4 +4,5 @@ use crate::domain::entities::user::User;
 
 pub trait UserRepository {
     fn create_user(&mut self, user: User) -> impl Future<Output = Result<(), Error>>;
+    fn count(&mut self) -> impl Future<Output = Result<i64, Error>>;
 }
--- src/insfractuture/persistence/postgresql_user_repository.rs
+++ src/insfractuture/persistence/postgresql_user_repository.rs
@@ -1,6 +1,28 @@
+use sqlx::{PgPool, query_scalar};
+
 use crate::domain::repository::user_repository::UserRepository;
 
-pub struct PostgresqlUserRepository {}
+pub struct PostgresqlUserRepository {
+    pub pool: PgPool,
+}
+
+impl PostgresqlUserRepository {
+    pub fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
+    async fn _count(&mut self) -> Result<i64, sqlx::Error> {
+        let count: i64 = query_scalar!(
+            r#"
+                SELECT COUNT(*) from users;
+            "#
+        )
+        .fetch_one(&self.pool)
+        .await?
+        .expect("somethisng went wrong");
+
+        Ok(count)
+    }
+}
 
 impl UserRepository for PostgresqlUserRepository {
     async fn create_user(
@@ -8,5 +30,41 @@ impl UserRepository for PostgresqlUserRepository {
         _: crate::domain::entities::user::User,
     ) -> Result<(), actix_web::Error> {
         todo!()
+    }
+
+    async fn count(&mut self) -> Result<i64, actix_web::Error> {
+        let count = self._count().await.expect("somethisng went wrong ");
+
+        Ok(count)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use dotenv::dotenv;
+    use sqlx::postgres::PgPoolOptions;
+
+    use crate::insfractuture::config::settings::Settings;
+
+    use super::*;
+
+    #[tokio::test]
+    #[ignore = "database test"]
+    async fn postgresql_user_repository_count_test() {
+        // cargo test postgresql_user_repository_count_test -- --ignored
+        dotenv().ok();
+        let settings = Settings::load();
+
+        let pool = PgPoolOptions::new()
+            .max_connections(10)
+            .connect(&settings.postgresql_url)
+            .await
+            .expect("not connected");
+
+        let mut repo = PostgresqlUserRepository::new(pool);
+
+        let count = repo.count().await.expect("no error");
+
+        assert_eq!(count, 0);
     }
 }
